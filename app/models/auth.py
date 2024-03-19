14d1e7921dbe210c6849b755023701743762ee0d
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2024-03-18 20:17:05 -0400

    require slightly more secure password

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, model_validator, EmailStr
+from pydantic import BaseModel, model_validator, EmailStr, field_validator
 
 
 class Token(BaseModel):
@@ -15,6 +15,30 @@ class SignUp(BaseModel):
     email: EmailStr
     password: str
     confirmPassword: str
+
+    @field_validator("password")
+    def password_secure(cls, value):
+        errors = []
+
+        if len(value) < 8:
+            errors.append("Password must be at least 8 characters long")
+
+        if not any(char.isdigit() for char in value):
+            errors.append("Password must contain at least one digit")
+
+        if not any(char.isalpha() for char in value):
+            errors.append("Password must contain at least one letter")
+
+        if not any(char.isupper() for char in value):
+            errors.append("Password must contain at least one uppercase letter")
+
+        if not any(char.islower() for char in value):
+            errors.append("Password must contain at least one lowercase letter")
+
+        if errors:
+            raise ValueError(errors)
+
+        return value
 
     @model_validator(mode="after")
     def passwords_match(self, values):
